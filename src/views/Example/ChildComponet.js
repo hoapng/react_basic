46b55d5dbe5f2fb9fa3afc855ddfcedf502bd550
hoapng <giahoa01giathieu@gmail.com>
2023-06-24 20:13:17 +0700

    arrow function

--- src/views/Example/ChildComponet.js
+++ src/views/Example/ChildComponet.js
@@ -1,46 +1,65 @@
 import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
-class Child extends React.Component {
-  // let name = '123';
-  state = {
-    firstName: '',
-    lastName: ''
-  }
-  handleChangeFirstName = (event) => {
-    this.setState({
-        firstName: event.target.value
-    })
-  }
-  handleChangeLastName = (event) => {
-    this.setState({
-        lastName: event.target.value
-    })
-  }
-  handleSubmit = (event) => {
-    event.preventDefault()
-    console.log('>>> check data input: ', this.state)
-    alert('click')
-  }
-  render() {
-    // let name = this.props.name;
-    // let age = this.props.age;
-    let {name, age, address, arrJobs} = this.props;
-    return (
-      <>
-        <div>Child: {name} - {age} - {address}</div>
-        <dic className = 'jobs-list'>
-          {
-            arrJobs.map((item, index) => {
-              return (
-                <div>
-                  {item.id}.{item.name}
-                </div>
-              )
-            })
-          }
-        </dic>
-      </>
-    );
-  }
+// class Child extends React.Component {
+//   // let name = '123';
+//   state = {
+//     firstName: '',
+//     lastName: ''
+//   }
+//   handleChangeFirstName = (event) => {
+//     this.setState({
+//         firstName: event.target.value
+//     })
+//   }
+//   handleChangeLastName = (event) => {
+//     this.setState({
+//         lastName: event.target.value
+//     })
+//   }
+//   handleSubmit = (event) => {
+//     event.preventDefault()
+//     console.log('>>> check data input: ', this.state)
+//     alert('click')
+//   }
+//   render() {
+//     // let name = this.props.name;
+//     // let age = this.props.age;
+//     let {name, age, address, arrJobs} = this.props;
+//     return (
+//       <>
+//         <div>Child: {name} - {age} - {address}</div>
+//         <dic className = 'jobs-list'>
+//           {
+//             arrJobs.map((item, index) => {
+//               return (
+//                 <div>
+//                   {item.id}.{item.name}
+//                 </div>
+//               )
+//             })
+//           }
+//         </dic>
+//       </>
+//     );
+//   }
+// }
+const Child = (props) => {
+  let {name, age, address, arrJobs} = props;
+  return (
+          <>
+            <div>Child: {name} - {age} - {address}</div>
+            <div className = 'jobs-list'>
+              {
+                arrJobs.map((item, index) => {
+                  return (
+                    <div>
+                      {item.id}.{item.name}
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </>
+        )
 }
 export default Child
