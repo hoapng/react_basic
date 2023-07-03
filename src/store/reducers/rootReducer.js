f95d2114769cbbf58f555defaef7dccff0c4cd96
hoapng <giahoa01giathieu@gmail.com>
2023-07-03 19:18:43 +0700

    add/ delete redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -5,7 +5,22 @@ const initState = {
         {id: 3, name: 'Nguyen'}
     ]
 }
+
 const rootReducer = (state = initState, action) => {
-    return state;
+    switch(action.type) {
+        case 'DELETE_USER':
+          console.log('check', action);
+
+          let users = state.users;
+          users = users.filter(item => item.id !== action.payload.id );
+          return {...state, users};
+        case 'ADD_USER':
+            let id = Math.floor(Math.random() * 10000);
+            let user = {id: id, name: `random - ${id}`}
+            return {...state, users: [...state.users, user]};
+        default:
+            return state;
+      }
+    
 }
 export default rootReducer
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -4,12 +4,34 @@ import logo from "../../assets/images/logo512.png"
 import { connect } from "react-redux";
 
 class Home extends React.Component {
+    handleDeletUser = (user) => {
+        // console.log('check', user)
+        this.props.deleteUserRedux(user)
+    }
+    handleAddUser = () => {
+        this.props.addUserRedux()
+    }
     render(){
-        console.log('check', this.props.dataRedux)
+        // console.log('check', this.props.dataRedux)
+        let listUsers = this.props.dataRedux;
+        
         return(
             <>
             <div>Hello from Hompage</div>
             <img src = {logo} style={{width: '30%', height: '30%'}}></img>
+            <div>
+                {listUsers && listUsers.length > 0 &&
+                listUsers.map((item, index) => {
+                    return (
+                        <div key={item.id}>
+                            {index + 1} - {item.name} 
+                            <button onClick={() => this.handleDeletUser(item)}>x</button>
+                            
+                        </div>
+                    )
+                })}
+                <button onClick={() => this.handleAddUser()}>Add</button>
+            </div>
             </>
         )
     }
@@ -17,4 +39,10 @@ class Home extends React.Component {
 const mapStateToProps = (state) => {
     return {dataRedux: state.users} 
 }
-export default connect(mapStateToProps)(Color(Home))
+const mapDiSpatchToDo = (dispatch) => {
+    return{
+        deleteUserRedux: (userDelete) => dispatch({type: 'DELETE_USER', payload: userDelete}),
+        addUserRedux: () => dispatch({type: 'ADD_USER'})
+    }
+}
+export default connect(mapStateToProps, mapDiSpatchToDo)(Color(Home))
